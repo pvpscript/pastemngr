48b93c9a517f0bd8c11144094d34b3866c4c98e6
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-05-06 16:58:13 -0300

    Requests handler

---
+++ pastebin_requests.py
@@ -0,0 +1,23 @@
+import requests
+
+class Pastebin():
+    def __init__(self, dev_key):
+        self.__dev_key = dev_key
+
+    def create_paste(self):
+        pass
+
+    def list_user_pastes(self):
+        pass
+
+    def delete_user_paste(self):
+        pass
+
+    def get_api_user_key(self):
+        pass
+
+    def get_user_info(self):
+        pass
+
+    def get_raw_paste(self):
+        pass
