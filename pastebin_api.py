7e59d88f2817dc9b996d1aaeb758819a376c8ef8
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-05-06 17:46:37 -0300

    API payload handler

--- pastebin_api.py
+++ pastebin_api.py
@@ -1,28 +1,24 @@
-import requests
-
 POST_URL = 'https://pastebin.com/api/api_post.php'
 LOGIN_URL = 'https://pastebin.com/api/api_login.php'
 RAW_URL = 'https://pastebin.com/api/api_raw.php'
 
 class Pastebin():
-    __payload = None
-
     def __init__(self, dev_key):
         self.__dev_key = dev_key
 
     def create_api_user_key(self, api_user_name, api_user_password):
-        # login
-        self.__payload = {
+        payload = {
                 'api_dev_key': self.__dev_key,
                 'api_user_name': api_user_name,
                 'api_user_password': api_user_password
         }
 
+        return (LOGIN_URL, payload)
+
     def create_paste(self, api_paste_code, api_user_key='',
                      api_paste_format='', api_paste_private='',
                      api_paste_expire_date=''):
-        # post
-        self.__payload = {
+        payload = {
                 'api_dev_key': self.__dev_key,
                 'api_option': 'paste',
                 'api_paste_code': api_paste_code,
@@ -32,43 +28,44 @@ class Pastebin():
                 'api_paste_expire_date': api_paste_expire_date
         }
 
+        return (POST_URL, payload)
+
     def list_user_pastes(self, api_user_key, api_results_limit=''):
-        # post
-        self.__payload = {
+        payload = {
                 'api_dev_key': self.__dev_key,
                 'api_option': 'list',
                 'api_user_key': api_user_key,
                 'api_results_limit': api_results_limit
         }
 
+        return (POST_URL, payload)
+
     def delete_user_paste(self, api_user_key, api_paste_key):
-        # post
-        self.__payload = {
+        payload = {
                 'api_dev_key': self.__dev_key,
                 'api_option': 'delete',
                 'api_user_key': api_user_key,
                 'api_paste_key': api_paste_key
         }
 
+        return (POST_URL, payload)
+
     def get_user_info(self, api_user_key):
-        # post
-        self.__payload = {
+        payload = {
                 'api_dev_key': self.__dev_key,
                 'api_option': 'userdetails',
                 'api_user_key': api_user_key
 
         }
 
+        return (POST_URL, payload)
+
     def get_raw_paste(self, api_user_key, api_paste_key):
-        # raw
-        self.__payload = {
+        payload = {
                 'api_dev_key': self.__dev_key,
                 'api_option': 'show_paste',
                 'api_user_key': api_user_key,
                 'api_paste_key': api_paste_key
         }
 
-
-    @property
-    def payload(self):
-        return self.__payload
+        return (RAW_URL, payload)
