1ee77f79f03689813d1398053ea91b53f4278068
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-05-06 19:23:53 -0300

    Added parsing methods

--- pastebin_api.py
+++ pastebin_api.py
@@ -1,3 +1,5 @@
+import xml.etree.ElementTree as ET
+
 POST_URL = 'https://pastebin.com/api/api_post.php'
 LOGIN_URL = 'https://pastebin.com/api/api_login.php'
 RAW_URL = 'https://pastebin.com/api/api_raw.php'
@@ -6,6 +8,23 @@ class Pastebin():
     def __init__(self, dev_key):
         self.__dev_key = dev_key
 
+    def __output_string(val):
+        return { 'content': val.decode('UTF-8') }
+
+    def __output_xml(val):
+        root = ET.fromstring('<root>' + val.decode('UTF-8') + '</root>')
+        
+        ret_dict = {'content': []}
+        for tag in root:
+            tmp_dict = {}
+            for e in tag:
+                tmp_dict[e.tag] = e.text
+            ret_dict['content'].append(tmp_dict)
+
+        return ret_dict
+
+            
+
     def create_api_user_key(self, api_user_name, api_user_password):
         payload = {
                 'api_dev_key': self.__dev_key,
@@ -13,7 +32,8 @@ class Pastebin():
                 'api_user_password': api_user_password
         }
 
-        return (LOGIN_URL, payload)
+
+        return (LOGIN_URL, payload, __output_string)
 
     def create_paste(self, api_paste_code, api_user_key='',
                      api_paste_format='', api_paste_private='',
@@ -28,7 +48,7 @@ class Pastebin():
                 'api_paste_expire_date': api_paste_expire_date
         }
 
-        return (POST_URL, payload)
+        return (POST_URL, payload, __output_string)
 
     def list_user_pastes(self, api_user_key, api_results_limit=''):
         payload = {
@@ -38,7 +58,7 @@ class Pastebin():
                 'api_results_limit': api_results_limit
         }
 
-        return (POST_URL, payload)
+        return (POST_URL, payload, __output_xml)
 
     def delete_user_paste(self, api_user_key, api_paste_key):
         payload = {
@@ -48,7 +68,7 @@ class Pastebin():
                 'api_paste_key': api_paste_key
         }
 
-        return (POST_URL, payload)
+        return (POST_URL, payload, __output_string)
 
     def get_user_info(self, api_user_key):
         payload = {
@@ -58,7 +78,7 @@ class Pastebin():
 
         }
 
-        return (POST_URL, payload)
+        return (POST_URL, payload, __output_xml)
 
     def get_raw_paste(self, api_user_key, api_paste_key):
         payload = {
@@ -68,4 +88,4 @@ class Pastebin():
                 'api_paste_key': api_paste_key
         }
 
-        return (RAW_URL, payload)
+        return (RAW_URL, payload, __output_string)
