7f83d85cfea922e7adbf565b1d7e7b6a183358c4
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-05-06 20:45:12 -0300

    post request

--- make_request.py
+++ make_request.py
@@ -15,3 +15,5 @@ def post(url, payload, parse_method):
 
     if bool(re.match(BAD_REQUEST_URL, response, re.I)):
         raise BadApiRequest(response[len(BAD_REQUEST_URL):])
+
+    return parse_method(response)
