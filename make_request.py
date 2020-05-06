b5443f3b9d6f868a8dd762a27d854a78dc6d0836
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-05-06 18:05:37 -0300

    Request handler

---
+++ make_request.py
@@ -0,0 +1,17 @@
+import re
+import requests
+
+BAD_REQUEST_STR = "Bad API request, "
+
+class BadApiRequest(Exception):
+    """ Raised when an API call returns an error """
+    pass
+
+def get():
+    pass
+
+def post(url, payload):
+    response = requests.post(url, payload).decode('utf-8')
+
+    if bool(re.match(BAD_REQUEST_URL, response, re.I)):
+        raise BadApiRequest(response[len(BAD_REQUEST_URL):])
