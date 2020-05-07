e9abfac3248c7f1e8ae90bc27173dcbf78be0f42
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-05-06 23:10:14 -0300

    Usage example

--- tpastebin.py
+++ tpastebin.py
@@ -1,38 +1,37 @@
 import os
 import argparse
-import requests
+from pastebin_api import Pastebin
+from make_request import post
 
 APP_NAME = 'tpastebin'
 
 HOME = os.getenv('HOME')
 XDG_CONFIG_HOME = os.getenv('XDG_CONFIG_HOME')
 
-POST_URL = 'https://pastebin.com/api/api_post.php'
-LOGIN_URL = 'https://pastebin.com/api/api_login.php'
-RAW_URL = 'https://pastebin.com/api/api_raw.php'
+def read_dev_key(): 
+    dev_key_file = HOME + '/.config/' + APP_NAME + '/dev_key' \
+            if XDG_CONFIG_HOME == None \
+            else XDG_CONFIG_HOME + '/' + APP_NAME + '/dev_key'
 
-class Config():
-    def __init__(self):
-        self._dev_key_file = HOME + '/.config/' + APP_NAME + '/dev_key' \
-                if XDG_CONFIG_HOME == None \
-                else XDG_CONFIG_HOME + '/' + APP_NAME + '/dev_key'
+    file_ref = open(dev_key_file, 'r')
 
-    def read_dev_key(self): 
-        file_ref = open(self._dev_key_file, 'r')
+    dev_key = file_ref.readline().rstrip('\r\n')
+    file_ref.close()
 
-        self._dev_key = file_ref.readline().rstrip('\r\n')
-        file_ref.close()
-
-        return self._dev_key 
+    return dev_key 
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog=APP_NAME,
                                      description='Pastebin on terminal.')
+
+    pastebin = Pastebin(read_dev_key())
+
+    login = input('login: ')
+    passwd = input('password: ')
+
+    user_key_data = pastebin.create_api_user_key(login, passwd)
+    user_key = post(*user_key_data)
     
-    conf = Config()
-
-    print(conf.read_dev_key())
-
-
+    print(user_key)
