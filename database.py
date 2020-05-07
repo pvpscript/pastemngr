781f5285956e59b511dd5d3e64126ef4b45f06b8
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-05-07 16:23:20 -0300

    Changed database resulting mechanism

--- database.py
+++ database.py
@@ -36,38 +36,12 @@ class User(Database):
     def read(self, username):
         result = self.cursor.execute(self.READ, (username,)).fetchone()
 
-        return {
-                'username': result['username'],
-                'user_key': result['user_key'],
-                'user_format_short': result['user_format_short'],
-                'user_expiration': result['user_expiration'],
-                'user_avatar_url': result['user_avatar_url'],
-                'user_private': result['user_private'],
-                'user_website': result['user_website'],
-                'user_email': result['user_email'],
-                'user_location': result['user_location'],
-                'user_account_type': result['user_account_type']
-        }
+        return dict(result) if result is not None else None
 
     def all(self):
         result = self.cursor.execute(self.ALL).fetchall()
-        res_list = []
 
-        for entry in result:
-            res_list.append({
-                'username': entry['username'],
-                'user_key': entry['user_key'],
-                'user_format_short': entry['user_format_short'],
-                'user_expiration': entry['user_expiration'],
-                'user_avatar_url': entry['user_avatar_url'],
-                'user_private': entry['user_private'],
-                'user_website': entry['user_website'],
-                'user_email': entry['user_email'],
-                'user_location': entry['user_location'],
-                'user_account_type': entry['user_account_type']
-            })
-
-        return res_list
+        return [dict(e) for e in result] if result is not None else None
 
 
 class PasteInfo(Database):
@@ -98,39 +72,13 @@ class PasteInfo(Database):
 
     def read(self, paste_key):
         result = self.cursor.execute(self.READ, (paste_key,)).fetchone()
-
-        return {
-                'paste_key': result['paste_key'],
-                'owner': result['owner'],
-                'paste_date': result['paste_date'],
-                'paste_size': result['paste_size'],
-                'paste_expire_date': result['paste_expire_date'],
-                'paste_private': result['paste_private'],
-                'paste_format_long': result['paste_format_long'],
-                'paste_format_short': result['paste_format_short'],
-                'paste_url': result['paste_url'],
-                'paste_hits': result['paste_hits']
-        }
+        
+        return dict(result) if result is not None else None
 
     def all(self):
         result = self.cursor.execute(self.ALL).fetchall()
-        res_list = []
 
-        for entry in result:
-            res_list.append({
-                'paste_key': entry['paste_key'],
-                'owner': entry['owner'],
-                'paste_date': entry['paste_date'],
-                'paste_size': entry['paste_size'],
-                'paste_expire_date': entry['paste_expire_date'],
-                'paste_private': entry['paste_private'],
-                'paste_format_long': entry['paste_format_long'],
-                'paste_format_short': entry['paste_format_short'],
-                'paste_url': entry['paste_url'],
-                'paste_hits': entry['paste_hits']
-            })
-
-        return res_list
+        return [dict(e) for e in result] if result is not None else None
 
 class PasteText(Database):
     CREATE = """
@@ -154,19 +102,9 @@ class PasteText(Database):
     def read(self, paste_key):
         result = self.cursor.execute(self.READ, (paste_key,)).fetchone()
 
-        return {
-                'paste_key': result['paste_key'],
-                'paste': result['paste']
-        }
+        return dict(result) if result is not None else None
 
     def all(self):
         result = self.cursor.execute(self.ALL).fetchall()
-        res_list = []
 
-        for entry in result:
-            res_list.append({
-                'paste_key': entry['paste_key'],
-                'paste': entry['paste']
-            })
-
-        return res_list
+        return [dict(e) for e in result] if result is not None else None
