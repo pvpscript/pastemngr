a32709abe46579252bd3c32c7e30425a0895a735
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-05-07 01:48:19 -0300

    db connection class

---
+++ connection.py
@@ -0,0 +1,33 @@
+import sys
+import sqlite3
+
+from config import Config
+
+class Connect:
+    __instance = None
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get_instance(self):
+        if self.__instance == None:
+            self.__instance = Connect()
+
+        return self.__instance
+
+    @classmethod
+    def get_connection(self):
+        connection = None
+
+        try:
+            url = Config.entry('tpastebin.db')
+
+            connection = sqlite3.connect(url)
+        except sqlite3.OperationalError:
+            print("Couldn't connect to database.", file=sys.stderr)
+
+        return connection
+
+
+
