9a11884eea243231ca7e645a1bbeaf47c42450ed
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-05-23 00:39:24 -0300

    Using os.path to concatenate paths. Added custom exception

--- pastemngr/core/config.py
+++ pastemngr/core/config.py
@@ -1,23 +1,33 @@
 import os
 
-class Config:
-    __home = os.getenv('HOME')
-    __xdg_config_home = os.getenv('XDG_CONFIG_HOME')
+class DevKeyNotFoundError(FileNotFoundError):
+    """ Dev key file not found """
 
-    __config_dir = self.__home + '/.config/pastemngr/' \
-            if __xdg_config_home == None \
-            else __xdg_config_home + '/pastemngr/'
+    def __init__(self, msg, orig_exception):
+        super().__init__(f'{msg}: {orig_exception}')
+        self.orig_exception = orig_exception
+        self.errno = 5 << 1
+
+class Config:
+    __home = os.environ.get('HOME')
+    __xdg_config_home = os.environ.get('XDG_CONFIG_HOME')
+
+    __config_dir = (
+            os.path.join(__home, '.config/pastemngr')
+            if __xdg_config_home == None
+            else os.path.join(__xdg_config_home, 'pastemngr')
+    )
 
     @classmethod
     def entry(self, name):
-        return self.__config_dir + name 
+        return os.path.join(self.__config_dir, name)
 
     @classmethod
     def read_dev_key(self):
         try:
             file_ref = open(self.entry('dev_key'), 'r')
         except FileNotFoundError:
-            raise FileNotFoundError('dev_key file not found, please create it and provide a developer key.')
+            raise DevKeyNotFoundError('dev_key file not found, please create it and provide a developer key.', e)
 
         dev_key = file_ref.readline().rstrip('\r\n')
         file_ref.close()
